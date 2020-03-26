3709c9749653264eee10506ff3a47d78697e97a4
Kyle Fuller <kyle@fuller.li>
2020-03-26 17:16:34 +0000

    style: remove unused variables

--- draughtsman/__init__.py
+++ draughtsman/__init__.py
@@ -1,6 +1,3 @@
-from ctypes.util import find_library
-from cffi import FFI
-from semantic_version import Version
 from refract.json import JSONDeserialiser
 from refract.contrib.apielements import registry, ParseResult
 from draughtsman.drafter import drafter_parse_blueprint_to
